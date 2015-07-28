ae2904f533e823e101aba2c706a3836e5af0ddca
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2015-07-28 17:33:34 +0900

    fixes of indent(tab to spaces) and eol(CRLF to LF).

--- php/sample_custom.php
+++ php/sample_custom.php
@@ -9,11 +9,11 @@ header("Content-type: text/xml");
 print("<qrcode>");
 
 for ($r = 0; $r < $qr->getModuleCount(); $r++) {
-	print("<line>");
-	for ($c = 0; $c < $qr->getModuleCount(); $c++) {
-		print($qr->isDark($r, $c)? "1" : "0");
-	}
-	print("</line>");
+    print("<line>");
+    for ($c = 0; $c < $qr->getModuleCount(); $c++) {
+        print($qr->isDark($r, $c)? "1" : "0");
+    }
+    print("</line>");
 }
 
 print("</qrcode>");
