bcb40090dbb627b5bc77a46e4e2b32bf5e27dab9
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-08-16 22:29:41 +0900

    just tab to spc.

--- js/sample.js
+++ js/sample.js
@@ -1,16 +1,17 @@
 
 var draw_qrcode = function(text, typeNumber, errorCorrectLevel) {
-	document.write(create_qrcode(text, typeNumber, errorCorrectLevel) );
+  document.write(create_qrcode(text, typeNumber, errorCorrectLevel) );
 };
 
 var create_qrcode = function(text, typeNumber, errorCorrectLevel, table) {
 
-	var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M');
-	qr.addData(text);
-	qr.make();
+  var qr = qrcode(typeNumber || 4, errorCorrectLevel || 'M');
+  qr.addData(text);
+  qr.make();
 
-//	return qr.createTableTag();
-	return qr.createImgTag();
+//  return qr.createTableTag();
+//  return qr.createSvgTag();
+  return qr.createImgTag();
 };
 
 var update_qrcode = function() {
@@ -19,5 +20,5 @@ var update_qrcode = function() {
     replace(/^[\s\u3000]+|[\s\u3000]+$/g, '');
   var t = form.elements['t'].value;
   var e = form.elements['e'].value;
-	document.getElementById('qr').innerHTML = create_qrcode(text, t, e);
+  document.getElementById('qr').innerHTML = create_qrcode(text, t, e);
 };
