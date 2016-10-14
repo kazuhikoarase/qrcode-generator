db5dfc3366caa994602ec372ef8bbb039c05fc2b
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2016-10-14 19:04:33 +0900

    commit

--- js/ts-test/src/import_test.ts
+++ js/ts-test/src/import_test.ts
@@ -1,9 +1,9 @@
 declare var require: any;
 declare var exports : any;
 
-import * as imported from "qrcode-generator";
+import * as imported from 'qrcode-generator';
 
-exports.reqImp = function(test : any) {
+exports.requireAndImport = function(test : any) {
 
   var testQR = function(qrcode : any) {
     test.equal(typeof qrcode, 'function');
