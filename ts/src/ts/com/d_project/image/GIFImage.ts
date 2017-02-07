42f58172a4535c701597db566b3e6ad1747a0d84
amtlib-dot-dll <amtlib-dot-dll@users.noreply.github.com>
2017-02-07 12:02:55 +0800

    Refactoring as https://github.com/Microsoft/TypeScript/wiki/FAQ#why-dont-namespaces-merge-across-different-module-files

--- ts/src/ts/com/d_project/image/GIFImage.ts
+++ ts/src/ts/com/d_project/image/GIFImage.ts
@@ -1,270 +1,263 @@
-/// <reference path="../io/OutputStream.ts" />
-/// <reference path="../io/ByteArrayOutputStream.ts" />
-/// <reference path="../io/Base64.ts" />
-'use strict';
-namespace com.d_project.image {
+import OutputStream from '../io/OutputStream';
+import ByteArrayOutputStream from '../io/ByteArrayOutputStream';
+import Base64 from '../io/Base64';
 
-  import OutputStream = com.d_project.io.OutputStream;
-  import ByteArrayOutputStream = com.d_project.io.ByteArrayOutputStream;
-  import Base64 = com.d_project.io.Base64;
-
-  /**
+/**
    * GIF Image (B/W)
    * @author Kazuhiko Arase
    */
-  export class GIFImage {
+export default class {
 
-    private width : number;
-    private height : number;
-    private data : number[];
+    private width: number;
+    private height: number;
+    private data: number[];
 
-    constructor(width : number, height : number) {
-      this.width = width;
-      this.height = height;
-      var size = width * height;
-      this.data = [];
-      for (var i = 0; i < size; i += 1) {
-        this.data.push(0);
-      }
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        var size = width * height;
+        this.data = [];
+        for (var i = 0; i < size; i += 1) {
+            this.data.push(0);
+        }
     }
 
-    public setPixel(x : number, y : number, pixel : number) : void {
-      if (x < 0 || this.width  <= x) throw '!' + x;
-      if (y < 0 || this.height <= y) throw '!' + y;
-      this.data[y * this.width + x] = pixel;
+    public setPixel(x: number, y: number, pixel: number): void {
+        if (x < 0 || this.width <= x) throw '!' + x;
+        if (y < 0 || this.height <= y) throw '!' + y;
+        this.data[y * this.width + x] = pixel;
     }
 
-    public getPixel(x : number, y : number) : number {
-      if (x < 0 || this.width  <= x) throw '!' + x;
-      if (y < 0 || this.height <= y) throw '!' + y;
-      return this.data[y * this.width + x];
+    public getPixel(x: number, y: number): number {
+        if (x < 0 || this.width <= x) throw '!' + x;
+        if (y < 0 || this.height <= y) throw '!' + y;
+        return this.data[y * this.width + x];
     }
 
-    public write(out : OutputStream) : void {
+    public write(out: OutputStream): void {
 
-      //---------------------------------
-      // GIF Signature
+        //---------------------------------
+        // GIF Signature
 
-      out.writeByte('G'.charCodeAt(0) );
-      out.writeByte('I'.charCodeAt(0) );
-      out.writeByte('F'.charCodeAt(0) );
-      out.writeByte('8'.charCodeAt(0) );
-      out.writeByte('7'.charCodeAt(0) );
-      out.writeByte('a'.charCodeAt(0) );
+        out.writeByte('G'.charCodeAt(0));
+        out.writeByte('I'.charCodeAt(0));
+        out.writeByte('F'.charCodeAt(0));
+        out.writeByte('8'.charCodeAt(0));
+        out.writeByte('7'.charCodeAt(0));
+        out.writeByte('a'.charCodeAt(0));
 
-      //---------------------------------
-      // Screen Descriptor
+        //---------------------------------
+        // Screen Descriptor
 
-      this.writeWord(out, this.width);
-      this.writeWord(out, this.height);
+        this.writeWord(out, this.width);
+        this.writeWord(out, this.height);
 
-      out.writeByte(0x80); // 2bit
-      out.writeByte(0);
-      out.writeByte(0);
+        out.writeByte(0x80); // 2bit
+        out.writeByte(0);
+        out.writeByte(0);
 
-      //---------------------------------
-      // Global Color Map
+        //---------------------------------
+        // Global Color Map
 
-      // black
-      out.writeByte(0x00);
-      out.writeByte(0x00);
-      out.writeByte(0x00);
+        // black
+        out.writeByte(0x00);
+        out.writeByte(0x00);
+        out.writeByte(0x00);
 
-      // white
-      out.writeByte(0xff);
-      out.writeByte(0xff);
-      out.writeByte(0xff);
+        // white
+        out.writeByte(0xff);
+        out.writeByte(0xff);
+        out.writeByte(0xff);
 
-      //---------------------------------
-      // Image Descriptor
+        //---------------------------------
+        // Image Descriptor
 
-      out.writeByte(','.charCodeAt(0) );
-      this.writeWord(out, 0);
-      this.writeWord(out, 0);
-      this.writeWord(out, this.width);
-      this.writeWord(out, this.height);
-      out.writeByte(0);
+        out.writeByte(','.charCodeAt(0));
+        this.writeWord(out, 0);
+        this.writeWord(out, 0);
+        this.writeWord(out, this.width);
+        this.writeWord(out, this.height);
+        out.writeByte(0);
 
-      //---------------------------------
-      // Local Color Map
+        //---------------------------------
+        // Local Color Map
 
-      //---------------------------------
-      // Raster Data
+        //---------------------------------
+        // Raster Data
 
-      var lzwMinCodeSize = 2;
-      var raster = this.getLZWRaster(lzwMinCodeSize);
+        var lzwMinCodeSize = 2;
+        var raster = this.getLZWRaster(lzwMinCodeSize);
 
-      out.writeByte(lzwMinCodeSize);
+        out.writeByte(lzwMinCodeSize);
 
-      var offset : number = 0;
+        var offset: number = 0;
 
-      while (raster.length - offset > 255) {
-        out.writeByte(255);
-        this.writeBytes(out, raster, offset, 255);
-        offset += 255;
-      }
-
-      out.writeByte(raster.length - offset);
-      this.writeBytes(out, raster, offset, raster.length - offset);
-      out.writeByte(0x00);
-
-      //---------------------------------
-      // GIF Terminator
-      out.writeByte(';'.charCodeAt(0) );
-    }
-
-    private getLZWRaster(lzwMinCodeSize : number) : number[] {
-
-      var clearCode = 1 << lzwMinCodeSize;
-      var endCode   = (1 << lzwMinCodeSize) + 1;
-      var bitLength = lzwMinCodeSize + 1;
-
-      // Setup LZWTable
-      var table = new LZWTable();
-
-      for (var i = 0; i < clearCode; i += 1) {
-        table.add(String.fromCharCode(i) );
-      }
-      table.add(String.fromCharCode(clearCode) );
-      table.add(String.fromCharCode(endCode) );
-
-      var byteOut = new ByteArrayOutputStream();
-      var bitOut  = new BitOutputStream(byteOut);
-
-      try {
-
-        // clear code
-        bitOut.write(clearCode, bitLength);
-
-        var dataIndex = 0;
-        var s = String.fromCharCode(this.data[dataIndex]);
-        dataIndex += 1;
-
-        while (dataIndex < this.data.length) {
-          var c = String.fromCharCode(this.data[dataIndex]);
-          dataIndex += 1;
-          if (table.contains(s + c) ) {
-              s = s + c;
-          } else {
-            bitOut.write(table.indexOf(s), bitLength);
-            if (table.getSize() < 0xfff) {
-              if (table.getSize() == (1 << bitLength) ) {
-                bitLength += 1;
-              }
-              table.add(s + c);
-            }
-            s = c;
-          }
+        while (raster.length - offset > 255) {
+            out.writeByte(255);
+            this.writeBytes(out, raster, offset, 255);
+            offset += 255;
         }
 
-        bitOut.write(table.indexOf(s), bitLength);
+        out.writeByte(raster.length - offset);
+        this.writeBytes(out, raster, offset, raster.length - offset);
+        out.writeByte(0x00);
 
-        // end code
-        bitOut.write(endCode, bitLength);
-
-      } finally {
-        bitOut.close();
-      }
-
-      return byteOut.toByteArray();
+        //---------------------------------
+        // GIF Terminator
+        out.writeByte(';'.charCodeAt(0));
     }
 
-    private writeWord(out : OutputStream, i : number) {
-      out.writeByte(i & 0xff);
-      out.writeByte( (i >>> 8) & 0xff);
+    private getLZWRaster(lzwMinCodeSize: number): number[] {
+
+        var clearCode = 1 << lzwMinCodeSize;
+        var endCode = (1 << lzwMinCodeSize) + 1;
+        var bitLength = lzwMinCodeSize + 1;
+
+        // Setup LZWTable
+        var table = new LZWTable();
+
+        for (var i = 0; i < clearCode; i += 1) {
+            table.add(String.fromCharCode(i));
+        }
+        table.add(String.fromCharCode(clearCode));
+        table.add(String.fromCharCode(endCode));
+
+        var byteOut = new ByteArrayOutputStream();
+        var bitOut = new BitOutputStream(byteOut);
+
+        try {
+
+            // clear code
+            bitOut.write(clearCode, bitLength);
+
+            var dataIndex = 0;
+            var s = String.fromCharCode(this.data[dataIndex]);
+            dataIndex += 1;
+
+            while (dataIndex < this.data.length) {
+                var c = String.fromCharCode(this.data[dataIndex]);
+                dataIndex += 1;
+                if (table.contains(s + c)) {
+                    s = s + c;
+                } else {
+                    bitOut.write(table.indexOf(s), bitLength);
+                    if (table.getSize() < 0xfff) {
+                        if (table.getSize() == (1 << bitLength)) {
+                            bitLength += 1;
+                        }
+                        table.add(s + c);
+                    }
+                    s = c;
+                }
+            }
+
+            bitOut.write(table.indexOf(s), bitLength);
+
+            // end code
+            bitOut.write(endCode, bitLength);
+
+        } finally {
+            bitOut.close();
+        }
+
+        return byteOut.toByteArray();
+    }
+
+    private writeWord(out: OutputStream, i: number) {
+        out.writeByte(i & 0xff);
+        out.writeByte((i >>> 8) & 0xff);
     }
 
     private writeBytes(
-      out : OutputStream,
-      bytes : number[], off : number, len : number
+        out: OutputStream,
+        bytes: number[], off: number, len: number
     ) {
-      for (var i = 0; i < len; i += 1) {
-        out.writeByte(bytes[i + off]);
-      }
+        for (var i = 0; i < len; i += 1) {
+            out.writeByte(bytes[i + off]);
+        }
     }
 
-    public toDataURL() : string {
-      var bout = new ByteArrayOutputStream();
-      this.write(bout);
-      bout.close();
-      var s = '';
-      var bytes = Base64.encode(bout.toByteArray() );
-      for (var i = 0; i < bytes.length; i += 1) {
-        s += String.fromCharCode(bytes[i]);
-      }
-      return 'data:image/gif;base64,' + s;
+    public toDataURL(): string {
+        var bout = new ByteArrayOutputStream();
+        this.write(bout);
+        bout.close();
+        var s = '';
+        var bytes = Base64.encode(bout.toByteArray());
+        for (var i = 0; i < bytes.length; i += 1) {
+            s += String.fromCharCode(bytes[i]);
+        }
+        return 'data:image/gif;base64,' + s;
     }
-  }
+}
 
-  class LZWTable {
+class LZWTable {
 
-    private map : { [key : string] : number; } = {};
-    private size : number = 0;
+    private map: { [key: string]: number; } = {};
+    private size: number = 0;
 
     constructor() {
     }
 
-    public add(key : string) : void {
-      if (this.contains(key) ) {
-        throw 'dup key:' + key;
-      }
-      this.map[key] = this.size;
-      this.size += 1;
+    public add(key: string): void {
+        if (this.contains(key)) {
+            throw 'dup key:' + key;
+        }
+        this.map[key] = this.size;
+        this.size += 1;
     }
 
-    public getSize() : number {
-      return this.size;
+    public getSize(): number {
+        return this.size;
     }
 
-    public indexOf(key : string) : number {
-      return this.map[key];
+    public indexOf(key: string): number {
+        return this.map[key];
     }
 
-    public contains(key : string) : boolean {
-      return typeof this.map[key] != 'undefined';
+    public contains(key: string): boolean {
+        return typeof this.map[key] != 'undefined';
     }
-  }
+}
 
-  class BitOutputStream {
+class BitOutputStream {
 
-    private out : OutputStream;
-    private bitLength : number;
-    private bitBuffer : number;
+    private out: OutputStream;
+    private bitLength: number;
+    private bitBuffer: number;
 
-    constructor(out : OutputStream) {
-      this.out = out;
-      this.bitLength = 0;
-    }
-
-    public write(data : number, length : number) : void {
-
-      if ( (data >>> length) != 0) {
-        throw 'length over';
-      }
-
-      while (this.bitLength + length >= 8) {
-        this.out.writeByte(0xff &
-          ( (data << this.bitLength) | this.bitBuffer) );
-        length -= (8 - this.bitLength);
-        data >>>= (8 - this.bitLength);
-        this.bitBuffer = 0;
+    constructor(out: OutputStream) {
+        this.out = out;
         this.bitLength = 0;
-      }
-
-      this.bitBuffer = (data << this.bitLength) | this.bitBuffer;
-      this.bitLength = this.bitLength + length;
     }
 
-    public flush() : void {
-      if (this.bitLength > 0) {
-        this.out.writeByte(this.bitBuffer);
-      }
-      this.out.flush();
+    public write(data: number, length: number): void {
+
+        if ((data >>> length) != 0) {
+            throw 'length over';
+        }
+
+        while (this.bitLength + length >= 8) {
+            this.out.writeByte(0xff &
+                ((data << this.bitLength) | this.bitBuffer));
+            length -= (8 - this.bitLength);
+            data >>>= (8 - this.bitLength);
+            this.bitBuffer = 0;
+            this.bitLength = 0;
+        }
+
+        this.bitBuffer = (data << this.bitLength) | this.bitBuffer;
+        this.bitLength = this.bitLength + length;
     }
 
-    public close() : void {
-      this.flush();
-      this.out.close();
+    public flush(): void {
+        if (this.bitLength > 0) {
+            this.out.writeByte(this.bitBuffer);
+        }
+        this.out.flush();
     }
-  }
+
+    public close(): void {
+        this.flush();
+        this.out.close();
+    }
 }
--- ts/src/ts/com/d_project/qrcode/Polynomial.ts
+++ ts/src/ts/com/d_project/qrcode/Polynomial.ts
@@ -1,98 +1,96 @@
-'use strict';
-namespace com.d_project.qrcode {
+import QRMath from './QRMath';
 
-  /**
-   * Polynomial
-   * @author Kazuhiko Arase
-   */
-  export class Polynomial {
+/**
+  * Polynomial
+  * @author Kazuhiko Arase
+  */
+export default class Polynomial {
 
-    private num : number[];
+    private num: number[];
 
-    public constructor(num : number[], shift = 0) {
+    public constructor(num: number[], shift = 0) {
 
-      var offset = 0;
+        var offset = 0;
 
-      while (offset < num.length && num[offset] == 0) {
-        offset += 1;
-      }
-
-      this.num = [];
-      var len = num.length - offset;
-      for (var i = 0; i < len; i += 1) {
-        this.num.push(num[offset + i]);
-      }
-      for (var i = 0; i < shift; i += 1) {
-        this.num.push(0);
-      }
-    }
-
-    public getAt(index : number) : number{
-      return this.num[index];
-    }
-
-    public getLength() : number {
-      return this.num.length;
-    }
-
-    public toString() : string {
-      var buffer = '';
-      for (var i = 0; i < this.getLength(); i += 1) {
-        if (i > 0) {
-          buffer += ',';
+        while (offset < num.length && num[offset] == 0) {
+            offset += 1;
         }
-        buffer += this.getAt(i);
-      }
-      return buffer.toString();
-    }
 
-    public toLogString() : string {
-      var buffer = '';
-      for (var i = 0; i < this.getLength(); i += 1) {
-        if (i > 0) {
-          buffer += ',';
+        this.num = [];
+        var len = num.length - offset;
+        for (var i = 0; i < len; i += 1) {
+            this.num.push(num[offset + i]);
         }
-        buffer += QRMath.glog(this.getAt(i) );
-      }
-      return buffer.toString();
-    }
-
-    public multiply(e : Polynomial) : Polynomial {
-      var num : number[] = [];
-      var len = this.getLength() + e.getLength() - 1;
-      for (var i = 0; i < len; i += 1) {
-        num.push(0);
-      }
-      for (var i = 0; i < this.getLength(); i += 1) {
-        for (var j = 0; j < e.getLength(); j += 1) {
-          num[i + j] ^= QRMath.gexp(QRMath.glog(this.getAt(i) ) +
-            QRMath.glog(e.getAt(j) ) );
+        for (var i = 0; i < shift; i += 1) {
+            this.num.push(0);
         }
-      }
-      return new Polynomial(num);
     }
 
-    public mod(e : Polynomial) : Polynomial {
-
-      if (this.getLength() - e.getLength() < 0) {
-        return this;
-      }
-
-      var ratio = QRMath.glog(this.getAt(0) ) - QRMath.glog(e.getAt(0) );
-
-      // create copy
-      var num : number[] = [];
-      for (var i = 0; i < this.getLength(); i += 1) {
-        num.push(this.getAt(i) );
-      }
-
-      // subtract and calc rest.
-      for (var i = 0; i < e.getLength(); i += 1) {
-        num[i] ^= QRMath.gexp(QRMath.glog(e.getAt(i) ) + ratio);
-      }
-
-      // call recursively
-      return new Polynomial(num).mod(e);
+    public getAt(index: number): number {
+        return this.num[index];
     }
-  }
+
+    public getLength(): number {
+        return this.num.length;
+    }
+
+    public toString(): string {
+        var buffer = '';
+        for (var i = 0; i < this.getLength(); i += 1) {
+            if (i > 0) {
+                buffer += ',';
+            }
+            buffer += this.getAt(i);
+        }
+        return buffer.toString();
+    }
+
+    public toLogString(): string {
+        var buffer = '';
+        for (var i = 0; i < this.getLength(); i += 1) {
+            if (i > 0) {
+                buffer += ',';
+            }
+            buffer += QRMath.glog(this.getAt(i));
+        }
+        return buffer.toString();
+    }
+
+    public multiply(e: Polynomial): Polynomial {
+        var num: number[] = [];
+        var len = this.getLength() + e.getLength() - 1;
+        for (var i = 0; i < len; i += 1) {
+            num.push(0);
+        }
+        for (var i = 0; i < this.getLength(); i += 1) {
+            for (var j = 0; j < e.getLength(); j += 1) {
+                num[i + j] ^= QRMath.gexp(QRMath.glog(this.getAt(i)) +
+                    QRMath.glog(e.getAt(j)));
+            }
+        }
+        return new Polynomial(num);
+    }
+
+    public mod(e: Polynomial): Polynomial {
+
+        if (this.getLength() - e.getLength() < 0) {
+            return this;
+        }
+
+        var ratio = QRMath.glog(this.getAt(0)) - QRMath.glog(e.getAt(0));
+
+        // create copy
+        var num: number[] = [];
+        for (var i = 0; i < this.getLength(); i += 1) {
+            num.push(this.getAt(i));
+        }
+
+        // subtract and calc rest.
+        for (var i = 0; i < e.getLength(); i += 1) {
+            num[i] ^= QRMath.gexp(QRMath.glog(e.getAt(i)) + ratio);
+        }
+
+        // call recursively
+        return new Polynomial(num).mod(e);
+    }
 }
