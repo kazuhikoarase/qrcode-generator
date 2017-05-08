b4eac73db89e20f7f7e7e7c53d65c6f546b4bd32
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2017-05-08 19:41:08 +0900

    commit

---
+++ java/src/main/java/com/d_project/qrcode/ErrorCorrectionLevel.java
@@ -0,0 +1,28 @@
+package com.d_project.qrcode;
+
+/**
+ * 誤り訂正レベル.
+ * @author Kazuhiko Arase 
+ */
+public interface ErrorCorrectionLevel {
+
+  /**
+   * 復元能力 7%.
+   */
+  int L = 1;
+  
+  /**
+   * 復元能力 15%.
+   */
+  int M = 0;
+
+  /**
+   * 復元能力 25%.
+   */
+  int Q = 3;
+
+  /**
+   * 復元能力 30%.
+   */
+  int H = 2;
+}
