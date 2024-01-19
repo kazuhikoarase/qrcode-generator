2ee1e1d25243d5561ac0b565e52e2a81bb9a1e52
kazuhikoarase <kazuhiko.arase@gmail.com>
2024-01-20 05:56:29 +0900

    GitHub Ribbons

--- js/demo/assets/sample.js
+++ js/demo/assets/sample.js
@@ -34,9 +34,11 @@ var create_qrcode = function(text, typeNumber,
   qr.addData(text, mode);
   qr.make();
 
+  console.log(qr.createASCII() );
+
 //  return qr.createTableTag();
-//  return qr.createSvgTag();
-  return qr.createImgTag();
+  return qr.createSvgTag();
+//  return qr.createImgTag();
 };
 
 var update_qrcode = function() {
